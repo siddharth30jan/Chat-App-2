8f23193813db61290bfc8af80bdf8468048ceffb
Siddharth <siddharths51700@gmail.com>
2018-10-31 21:55:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,17 +8,32 @@ const server=http.createServer(app);
 const io=socketio(server)
 app.use('/',express.static(path.join(__dirname,'frontend')));
 
+let storage={}
+
 io.on('connection',(socket)=>{
     console.log("New socket formed from "+socket.id)
     socket.emit('connected')
 
+   
+    socket.on('send_msg',(data)=>{
+        storage[data.user]=socket.id
+    })
 
     socket.on('send_msg',(data)=>{
         //console.log("Recieved Message = " + data.message)
 
         //io.emit  sabko jayega including ME
         //socket.broadcast se sirf dusron ko jayega!!
-        socket.broadcast.emit('recv_msg', data);
+        if(data.message.startsWith('@'))
+        {
+            let temp=data.message.split(':')[0].substr(1)
+            let send_socketid=storage[temp]
+           io.to(send_socketid).emit('recv_msg',data)
+
+        }
+        else{
+            socket.broadcast.emit('recv_msg', data);
+        }
     })
 })
 
